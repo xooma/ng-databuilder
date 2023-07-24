bee8033b3e652ec9b7193ffc0355d1e7935a909a
MaximilienB <xoomabis@gmail.com>
2023-07-24 20:13:19 +0200

    rollback: Undesired files commited

--- lib/factory.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Factory<T> {
-    private object;
-    constructor(objectConstructor: new () => T);
-    withProperties(properties: Partial<T>): Factory<T>;
-    build(): T;
-}
-declare function makeFactory<T>(objectConstructor: new () => T): Factory<T>;
-export { makeFactory };
